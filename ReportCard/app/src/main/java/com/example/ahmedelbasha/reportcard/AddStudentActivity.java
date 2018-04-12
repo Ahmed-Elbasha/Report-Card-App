84f48550e43ab7b089d262f04396f9cffea32cbc
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-04-12 17:47:53 +0200

    Added Custom Action Bars Menus to Activities

--- ReportCard/app/src/main/java/com/example/ahmedelbasha/reportcard/AddStudentActivity.java
+++ ReportCard/app/src/main/java/com/example/ahmedelbasha/reportcard/AddStudentActivity.java
@@ -2,6 +2,10 @@ package com.example.ahmedelbasha.reportcard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AddStudentActivity extends AppCompatActivity {
 
@@ -9,5 +13,35 @@ public class AddStudentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_modify_student_grade_activities_menu, menu);
+        MenuItem commitEditMenuItem = menu.findItem(R.id.edit_action);
+        commitEditMenuItem.getActionView();
+        commitEditMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                String message = "Commit Edit Action is clicked";
+                Toast testToast = Toast.makeText(AddStudentActivity.this, message, Toast.LENGTH_SHORT);
+                testToast.show();
+                return true;
+            }
+        });
+
+        MenuItem cancelEditMenuItem = menu.findItem(R.id.cancel_action);
+        cancelEditMenuItem.getActionView();
+        cancelEditMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                String message = "Cancel Edit Action is clicked";
+                Toast testToast = Toast.makeText(AddStudentActivity.this, message, Toast.LENGTH_SHORT);
+                testToast.show();
+                return true;
+            }
+        });
+        return  true;
     }
 }
--- ReportCard/app/src/main/java/com/example/ahmedelbasha/reportcard/MainActivity.java
+++ ReportCard/app/src/main/java/com/example/ahmedelbasha/reportcard/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.ahmedelbasha.reportcard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_activity_menu, menu);
+
+        MenuItem addNewStudentGradeMenuItem = menu.findItem(R.id.create_student_action);
+        addNewStudentGradeMenuItem.getActionView();
+        addNewStudentGradeMenuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent navigateToAddStudentActivityIntent = new Intent(MainActivity.this, AddStudentActivity.class);
+                startActivity(navigateToAddStudentActivityIntent);
+                return true;
+            }
+        });
+        return true;
     }
 }
